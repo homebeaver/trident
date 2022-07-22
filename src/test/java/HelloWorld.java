f61811ed6b8889eadc09e3321a5c32fce0c129a5
homebeaver <homebeaver@outlook.de>
2022-07-22 18:26:38 +0200

    HelloWorld + SimpleProp to maven test dir

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,9 +1,37 @@
+/*
+ * Copyright (c) 2005-2018 Trident Kirill Grouchnikov. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  o Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer. 
+ *     
+ *  o Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution. 
+ *     
+ *  o Neither the name of Trident Kirill Grouchnikov nor the names of 
+ *    its contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission. 
+ *     
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
 import org.pushingpixels.trident.Timeline;
 
 public class HelloWorld {
     private float value;
 
-    // Note that the actual numbers depend on the current system load.
     public void setValue(float newValue) {
         System.out.println(this.value + " -> " + newValue);
         this.value = newValue;
@@ -13,11 +41,8 @@ public class HelloWorld {
         HelloWorld helloWorld = new HelloWorld();
         Timeline timeline = new Timeline(helloWorld);
         timeline.addPropertyToInterpolate("value", 0.0f, 1.0f);
-        System.out.println("default Duration = " + timeline.getDuration());
         timeline.play();
 
-        // make sure that the application waits long enough for the timeline to finish playing
-        // long enough? 120 % of default Duration or more   
         try {
             Thread.sleep(3000);
         } catch (Exception exc) {
